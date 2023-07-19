41f602b373f21126153e79a5edf26e8d1aafc614
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-07-19 12:29:19 +0300

    updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Blueprint, get_flashed_messages
+from models import app, User, db
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():

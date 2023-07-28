81114cd2c24b12dff3d3ad08d2f2e0306d45fdae
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-07-28 13:48:26 +0300

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# CALU
-Creating a fully functional website that creates a centralized platform for all school clubs
--- mysql.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE calu;
-CREATE USER 'calu_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON calu.* TO 'calu_user'@'localhost';
-FLUSH PRIVILEGES;

dddf6f625c6ca422663112c3678fddb96184cbb3
JoakBouy <b.joak@alustudent.com>
2023-08-03 01:25:19 +0300

    Enabled filtering, Allowed ordering profiles

--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -1,13 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, filters
 from .serializers import UserProfileSerializer  
 from .models import UserProfile
 from .permissions import IsOwnerOrReadOnly
 
 class UserProfileList(generics.ListCreateAPIView):
-    queryset = UserProfile.objects.all()
+    
+    queryset = UserProfile.objects.all()    
     serializer_class = UserProfileSerializer
+    
+    filter_backends = [filters.SearchFilter]  
+    search_fields = ['name', 'username']
+      
+    ordering_fields = ['name', 'username']
+    
 
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+      
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+      
     permission_classes = [IsOwnerOrReadOnly]

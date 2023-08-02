2e5add252b743d067b73d020ec49b330b2f1b271
JoakBouy <b.joak@alustudent.com>
2023-08-03 01:19:24 +0300

    Import profile update permissions class

--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .serializers import UserProfileSerializer  
 from .models import UserProfile
+from .permissions import IsOwnerOrReadOnly
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -9,3 +10,4 @@ class UserProfileList(generics.ListCreateAPIView):
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]

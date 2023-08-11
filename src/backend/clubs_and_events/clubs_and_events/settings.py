fe15723977a02bfdd6d2830357e14522b33bbb5f
JoakBouy <b.joak@alustudent.com>
2023-08-12 02:39:57 +0300

    Configured installed apps

--- src/backend/clubs_and_events/clubs_and_events/settings.py
+++ src/backend/clubs_and_events/clubs_and_events/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'community',
+    'asset',
+    'membership',
+    'category',
+    'core',
+    'user',
+    'corsheaders',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
